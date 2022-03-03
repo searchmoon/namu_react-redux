84d06737bc3179823c74805d153dac2eb21d234a
searchmoon <moonding901212@gmail.com>
2022-03-03 22:38:03 +0900

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HeaderNavbar from './HeaderNavbar';
+
+function Header() {
+
+  return (
+    <div className="header">
+      <div>
+        <h1>느린나무2호점</h1>
+        <span>test</span>
+      </div>
+    </div> 
+  )
+}
+
+export default Header;
