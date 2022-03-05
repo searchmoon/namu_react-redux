ce5f0c810f6e4daefbc471de56159466918a103f
searchmoon <moonding901212@gmail.com>
2022-03-05 22:53:17 +0900

    Coffee.js 생성

---
+++ src/components/Coffee.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Coffee() {
+    return (
+        <div>
+            <h3>Coffee</h3>
+            <div>
+                <div>
+                    <span>아메리카노</span>
+                    <div>
+                        <button>ICE</button>
+                        <button>HOT</button>
+                    </div>
+                </div>
+                <div>
+                    <span>카페라떼</span>
+                    <div>
+                        <button>ICE</button>
+                        <button>HOT</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Coffee; 
