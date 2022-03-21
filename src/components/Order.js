f0098e322a1578b055b02d787df69b0a0676f476
searchmoon <moonding901212@gmail.com>
2022-03-21 23:55:56 +0900

    컴포넌트 추가

---
+++ src/components/Order.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Order() {
+  return(
+    <div>
+      <Link to="order" style={{ color: gray }}>
+        moon901212
+      </Link>
+    </div>
+  )
+}
+
+export default Order;
