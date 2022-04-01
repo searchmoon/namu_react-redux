791b1d59f0b45e03d9dfdb9b05016edbdbff3841
searchmoon <moonding901212@gmail.com>
2022-04-01 18:17:04 +0900

    Link 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
-import React from 'react';
-// import { Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
+import Order from "./Order";
 
 function Footer() {
   return (
     <div>
-      {/* <Link to="www.naver.com">moon901212@gmail.com</Link> */}
-    <a href="www.naver.com">moon901212</a>
-    </div> 
-  )
+      <Link to="/mail">moon901212@gmail.com</Link>
+      <p>Copyright Ⓒ 문정은 2022. All Rights Reserved.</p>
+    </div>
+  );
 }
 
 export default Footer;
