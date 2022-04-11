5406c6b2355dcb36b6af370ad7161354a72917a1
searchmoon <moonding901212@gmail.com>
2022-04-11 18:11:38 +0900

    Routes 생성

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import Order from "./components/Order";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />}/>
+        <Route path="/order" element={<Order />}/>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
